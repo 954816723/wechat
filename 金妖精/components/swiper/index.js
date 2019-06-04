9829dc0b5e18346b0424d656b597c4138d7a9045
chenwei <954816723@qq.com>
2019-06-04 18:39:10 +0800

    wechat

---
+++ 金妖精/components/swiper/index.js
@@ -0,0 +1,27 @@
+Component({
+    properties:{
+        autoplay:{
+            type:Boolean,
+            value:true
+        },
+        interval:{
+            type:Number,
+            value:5000
+        },
+        duration:{
+            type:Number,
+            value:1000
+        },
+        imgUrls:{
+            type:Array,
+            value:[]
+        }
+
+    },
+    data:{
+
+    },
+    methods:{
+        
+    }
+})
